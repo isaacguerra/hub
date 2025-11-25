94d8f823695e8d91d7bc72a0b66b86af4e9a8f17
Isaac Guerra <guerra.isaac@gmail.com>
2025-11-25 14:55:45 -0300

    correcoes

--- app/controllers/mobile/apoiadores_controller.rb
+++ app/controllers/mobile/apoiadores_controller.rb
@@ -1,9 +1,8 @@
 module Mobile
   class ApoiadoresController < BaseController
     before_action :set_apoiador, only: %i[show edit update destroy]
-    before_action :authorize_create, only: %i[new create]
     before_action :authorize_manage, only: %i[edit update destroy]
-    before_action :load_auxiliary_data, only: %i[new create edit update]
+    before_action :load_auxiliary_data, only: %i[edit update]
 
     def index
       @apoiadores = scope_apoiadores.includes(:funcao, :municipio, :regiao, :bairro, :lider).order(:nome)
@@ -13,32 +12,12 @@ module Mobile
     def show
     end
 
-    def new
-      @apoiador = Apoiador.new
-    end
-
-    def create
-      @apoiador = Apoiador.new(apoiador_params)
-      # Se quem cria é líder ou coordenador, define ele como líder do novo apoiador,
-      # a menos que seja admin definindo outro líder (mas vamos simplificar)
-      @apoiador.lider = Current.apoiador unless Current.apoiador.candidato? || Current.apoiador.coordenador_geral?
-
-      # Define senha padrão ou gera aleatória se necessário, mas aqui vamos assumir que o model lida ou é via magic link depois
-      # Mas para salvar precisa de validação? Vamos tentar salvar.
-
-      if @apoiador.save
-        redirect_to mobile_apoiadores_path, notice: "Apoiador cadastrado com sucesso."
-      else
-        render :new, status: :unprocessable_entity
-      end
-    end
-
     def edit
     end
 
     def update
       if @apoiador.update(apoiador_params)
-        redirect_to mobile_apoiadores_path, notice: "Apoiador atualizado com sucesso."
+        redirect_to mobile_apoiador_path(@apoiador), notice: "Apoiador atualizado com sucesso."
       else
         render :edit, status: :unprocessable_entity
       end
@@ -74,18 +53,14 @@ module Mobile
     end
 
     def apoiador_params
-      params.require(:apoiador).permit(:nome, :email, :whatsapp, :nascimento, :cep, :endereco, :numero, :complemento, :bairro_id, :municipio_id)
+      permitted = [ :nome, :email, :whatsapp, :nascimento, :cep, :endereco, :numero, :complemento, :bairro_id, :municipio_id ]
+      permitted << :funcao_id if Current.apoiador.candidato? || Current.apoiador.coordenador_geral?
+      params.require(:apoiador).permit(permitted)
     end
 
     def load_auxiliary_data
       @bairros = Bairro.order(:nome)
       @municipios = Municipio.order(:nome)
-    end
-
-    def authorize_create
-      unless Current.apoiador.pode_coordenar? || Current.apoiador.lider?
-        redirect_to mobile_apoiadores_path, alert: "Você não tem permissão para cadastrar apoiadores."
-      end
     end
 
     def authorize_manage
